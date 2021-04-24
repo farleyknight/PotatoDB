779ace911038f116a7f782cbfee230c7be3e420c
Farley Knight <farleyknight@gmail.com>
2021-04-24 18:30:09 -0400

    Add stubs for both tests

---
+++ tests/parser_test.hpp
@@ -0,0 +1,11 @@
+
+
+
+TEST(ParserTest, DISABLED_SimpleSelectTest) {
+  // TODO: Test using SQLParser::as_exprs()
+}
+
+
+TEST(ParserTest, DISABLED_InsertTest) {
+  // TODO: Test using SQLParser::as_exprs()
+}
